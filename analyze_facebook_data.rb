444f9891f895cdd4b17c77645b7b8f51f212fc7d
Eric Bartholemy <eric@kabisa.nl>
2018-04-13 17:41:44 +0200

    start refactor, extract into classes, add comments to create orientation

--- analyze_facebook_data.rb
+++ analyze_facebook_data.rb
@@ -53,12 +53,16 @@ user_name = Nokogiri::HTML(File.open("#{catalog}/index.htm")).title.split(' - Pr
 Dir.chdir("#{catalog}/messages") do
   messages_files = Dir.glob('*.html')
 
+  # AnalyzeMessages
   messages_files.each do |file|
+    # open
     content = File.open(file)
-
     doc = Nokogiri::HTML(content)
+
+    # get friend name
     friend_name = doc.title.split('Conversation with ')[1]
 
+    # return value
     friends[friend_name] ||= {
       you_count: 0,
       you_characters: 0,
@@ -69,47 +73,66 @@ Dir.chdir("#{catalog}/messages") do
       total_count: 0
     }
 
+    # Debug
     puts "Analyzing conversation with: #{friend_name}"
 
+    # whole conversation
     conversation = doc.css('.thread').children
 
+    # instance
     current_message_sender = ''
-    conversation.each do |conversation_node|
-      if conversation_node.name == 'div' && conversation_node['class'] == 'message'
-        current_message_sender = conversation_node.css('span.user').text
-        date_info = conversation_node.css('span.meta').text
 
+    conversation.each do |conversation_node|
+
+      # is the converstation a message?
+      if conversation_node.name == 'div' && conversation_node['class'] == 'message'
+        # user sending this message
+        current_message_sender  = conversation_node.css('span.user').text
+        # meta conversation data
+        date_info               = conversation_node.css('span.meta').text
+
+        # who sent message, was this me or you
         if current_message_sender == user_name
-          me[:total_message_count] += 1
-          friends[friend_name][:you_count] += 1
-          friends[friend_name][:total_count] += 1
+          # me
+          me[:total_message_count]            += 1
+          friends[friend_name][:you_count]    += 1
+          friends[friend_name][:total_count]  += 1
           my_messages_dates << Time.parse(date_info)
         else
+          # a friend of mine
           friends[friend_name][:total_count] += 1
           friends[friend_name][:friend_count] += 1
         end
       end
 
+      # skip this conversation node
       next unless conversation_node.name == 'p'
-      paragraph = conversation_node.text.downcase
-      paragraph = paragraph.delete(',').delete('.')
+
+      # numbers about words in the conversation
+      paragraph        = conversation_node.text.downcase
+      paragraph        = paragraph.delete(',').delete('.')
       paragraph_length = paragraph.length
-      paragraph_words = paragraph.split(' ')
+      paragraph_words  = paragraph.split(' ')
 
+      # who sent this message, me or you?
       if current_message_sender == user_name
-        me[:total_characters] += paragraph_length
-        me[:total_words] += paragraph_words.length
-        me[:total_xd] += paragraph.scan('xd').length
+        # me
+        me[:total_characters]   += paragraph_length
+        me[:total_words]        += paragraph_words.length
+        me[:total_xd]           += paragraph.scan('xd').length
 
+        # build dictionary
         paragraph_words.each do |word|
           dictionary[word] += 1
         end
 
+        # conversation with friend
         friends[friend_name][:you_characters] += paragraph_length
-        friends[friend_name][:you_words] += paragraph_words.length
+        friends[friend_name][:you_words]      += paragraph_words.length
       else
-        friends[friend_name][:friend_characters] += paragraph_length
-        friends[friend_name][:friend_words] += paragraph_words.length
+        # you
+        friends[friend_name][:friend_characters]  += paragraph_length
+        friends[friend_name][:friend_words]       += paragraph_words.length
       end
     end
   end
@@ -117,6 +140,7 @@ end
 
 ranking = friends.sort_by { |_name, friend| friend[:total_count] }.reverse
 
+# CreatePackage
 package = Axlsx::Package.new
 package.workbook.add_worksheet(name: 'Friends ranking') do |sheet|
   sheet.add_row ['Friends ranking']
@@ -133,6 +157,45 @@ package.workbook.add_worksheet(name: 'Friends ranking') do |sheet|
 end
 
 # analyze message patterns when messages are sent
+class MessagesSent
+  attr_accessor :by_month, :by_year, :by_day_of_week,
+                :by_weekend, :by_date, :by_hour,
+                :by_year_hour
+  attr_reader :my_messages_dates
+
+  def initialize(my_messages_dates)
+    @my_messages_dates = my_messages_dates
+    @by_month       = Hash.new(0)
+    @by_year        = Hash.new(0)
+    @by_day_of_week = Hash.new(0)
+    @by_weekend     = Hash.new(0)
+    @by_date        = Hash.new(0)
+    @by_hour        = Hash.new(0)
+    @by_year_hour   = Hash.new(0)
+  end
+
+  def build
+    my_messages_dates.each do |date|
+      by_month[date.strftime('%B')] += 1
+      by_year[date.year] += 1
+      by_day_of_week[date.strftime('%A')] += 1
+
+      if date.friday? || date.saturday? || date.sunday?
+        by_weekend[:weekend] += 1
+      else
+        by_weekend[:working] += 1
+      end
+
+      by_date[date.strftime('%F')] += 1
+      by_hour[date.hour] += 1
+      by_year_hour["#{date.year} - #{date.hour}"] += 1
+    end
+    self
+  end
+end
+
+messages_sent = MessagesSent.new(my_messages_dates).build
+
 package.workbook.add_worksheet(name: 'My message statistics') do |sheet|
   sheet.add_row ['My message statistics']
   sheet.add_row ["You sent in total #{me[:total_message_count]} messages"]
@@ -141,69 +204,52 @@ package.workbook.add_worksheet(name: 'My message statistics') do |sheet|
   sheet.add_row ["You also happened to use xD #{me[:total_xd]} times"]
   sheet.add_row ['']
 
-  by_month = Hash.new(0)
-  by_year = Hash.new(0)
-  by_day_of_week = Hash.new(0)
-  by_weekend = Hash.new(0)
-  by_date = Hash.new(0)
-  by_hour = Hash.new(0)
-  by_year_hour = Hash.new(0)
-
-  my_messages_dates.each do |date|
-    by_month[date.strftime('%B')] += 1
-    by_year[date.year] += 1
-    by_day_of_week[date.strftime('%A')] += 1
-
-    if date.friday? || date.saturday? || date.sunday?
-      by_weekend[:weekend] += 1
-    else
-      by_weekend[:working] += 1
-    end
-
-    by_date[date.strftime('%F')] += 1
-    by_hour[date.hour] += 1
-    by_year_hour["#{date.year} - #{date.hour}"] += 1
-  end
-
   sheet.add_row ['Messaging by month']
   sheet.add_row ['Month', 'number of messages']
-  by_month.sort_by { |_month, count| count }.each do |month, count|
+
+  messages_sent.by_month.sort_by { |_month, count| count }.each do |month, count|
     sheet.add_row [month, count]
   end
 
   sheet.add_row ['Messaging by year']
   sheet.add_row ['Year', 'number of messages']
-  by_year.sort_by { |year, _count| year }.each do |year, count|
+
+  messages_sent.by_year.sort_by { |year, _count| year }.each do |year, count|
     sheet.add_row [year, count]
   end
 
   sheet.add_row ['Messaging by day of week']
   sheet.add_row ['Day of week', 'number of messages']
-  by_day_of_week.sort_by { |_day, count| count }.reverse.each do |day, count|
+
+  messages_sent.by_day_of_week.sort_by { |_day, count| count }.reverse.each do |day, count|
     sheet.add_row [day, count]
   end
 
   sheet.add_row ['Messaging on week days vs. weekend']
   sheet.add_row ['Type of day', 'number of messages']
-  by_weekend.each do |type, count|
+
+  messages_sent.by_weekend.each do |type, count|
     sheet.add_row [type, count]
   end
 
   sheet.add_row ['Breakdown of messages by hour']
   sheet.add_row ['Hour', 'number of messages']
-  by_hour.sort_by { |hour, _count| hour }.each do |hour, count|
+
+  messages_sent.by_hour.sort_by { |hour, _count| hour }.each do |hour, count|
     sheet.add_row [hour, count]
   end
 
   sheet.add_row ['Breakdown of messages by hour and year']
   sheet.add_row ['Year and hour', 'number of messages']
-  by_year_hour.sort_by { |year_hour, _count| y, h = year_hour.split(' - '); "#{y}0".to_i + h.to_i }.each do |year_hour, count|
+
+  messages_sent.by_year_hour.sort_by { |year_hour, _count| y, h = year_hour.split(' - '); "#{y}0".to_i + h.to_i }.each do |year_hour, count|
     sheet.add_row [year_hour, count]
   end
 
   sheet.add_row ['Most busy messaging days']
   sheet.add_row ['Date', 'number of messages']
-  by_date.sort_by { |_date, count| count }.reverse.each do |date, count|
+
+  messages_sent.by_date.sort_by { |_date, count| count }.reverse.each do |date, count|
     sheet.add_row [date, count]
   end
 end
@@ -258,55 +304,103 @@ Dir.chdir("#{catalog}/html/") do
   end
 end
 
-# analyze making of friends
-friends_dates = []
-Dir.chdir("#{catalog}/html/") do
-  content = File.open('friends.htm').read
-  doc = Nokogiri::HTML(content)
-  friends_list = doc.css('div.contents > ul')[0].css('li')
+class FriendsDates
+  # analyze making of friends
+  # Returns an array of [<Date>,<Date>]
 
-  friends_list.each do |friend_element|
-    friend_with_email = friend_element.text.match(/(.*)\s\((.*)\)\s\((.*)\)/)
-    if friend_with_email
-      name, date_added = friend_with_email.captures
-    else
-      name, date_added = friend_element.text.match(/(.*)\s\((.*)\)/).captures
+  attr_reader :file
+  attr_accessor :friends_dates
+
+  def self.analyze(catalog)
+    new(catalog).analyze
+  end
+
+  def initialize(catalog)
+    @friends_dates = []
+    @file = "#{catalog}/html/friends.htm"
+  end
+
+  def analyze
+    friends_list.each do |friend_element|
+      if friend_with_email(friend_element)
+        _name, date_added = friend_with_email(friend_element).captures
+      else
+        _name, date_added = friend_element.text
+                                          .match(/(.*)\s\((.*)\)/)
+                                          .captures
+      end
+
+      date = if date_added == 'Today'
+               Date.today
+             elsif date_added == 'Yesterday'
+               Date.today.prev_day
+             else
+               Date.parse(date_added)
+             end
+
+      friends_dates << date
     end
-    date = if date_added == 'Today'
-             Date.today
-           elsif date_added == 'Yesterday'
-             Date.today.prev_day
-           else
-             Date.parse(date_added)
-           end
+    self
+  end
 
-    friends_dates << date
+  def content
+    File.open(file).read
+  end
+
+  def doc
+    Nokogiri::HTML(content)
+  end
+
+  def friends_list
+    doc.css('div.contents > ul')[0].css('li')
+  end
+
+  def friend_with_email(friend_element)
+    friend_element.text.match(/(.*)\s\((.*)\)\s\((.*)\)/)
   end
 end
 
-by_year = Hash.new(0)
-by_week_day = Hash.new(0)
-by_day = Hash.new(0)
-by_month = Hash.new(0)
-by_month_and_year = Hash.new(0)
-by_weekend = Hash.new(0)
-by_weekend_and_year = Hash.new(0)
-by_week_and_year = Hash.new(0)
+class MakingFriendsData
+  # Creates the MakingFriendsData with help of FriendsDates
+  attr_accessor :by_year, :by_week_day, :by_day,
+              :by_month, :by_month_and_year, :by_weekend,
+              :by_week_and_year
+  attr_reader :friends_dates
 
-friends_dates.each do |date|
-  by_year[date.year] += 1
-  by_week_day[date.strftime('%A')] += 1
-  by_day[date.strftime('%F')] += 1
-  by_month[date.strftime('%B')] += 1
-  by_month_and_year[date.strftime('%B - %Y')] += 1
-  by_week_and_year["week #{date.cweek} of #{date.year}"] += 1
+  def initialize(friends_dates_data)
+    @friends_dates         = friends_dates_data
 
-  if date.friday? || date.saturday? || date.sunday?
-    by_weekend[:weekend] += 1
-  else
-    by_weekend[:working] += 1
+    @by_year               = Hash.new(0)
+    @by_week_day           = Hash.new(0)
+    @by_day                = Hash.new(0)
+    @by_month              = Hash.new(0)
+    @by_month_and_year     = Hash.new(0)
+    @by_weekend            = Hash.new(0)
+    # @by_weekend_and_year = Hash.new(0)
+    @by_week_and_year      = Hash.new(0)
+  end
+
+  def build
+    friends_dates.each do |date|
+      by_year[date.year] += 1
+      by_week_day[date.strftime('%A')] += 1
+      by_day[date.strftime('%F')] += 1
+      by_month[date.strftime('%B')] += 1
+      by_month_and_year[date.strftime('%B - %Y')] += 1
+      by_week_and_year["week #{date.cweek} of #{date.year}"] += 1
+
+      if date.friday? || date.saturday? || date.sunday?
+        by_weekend[:weekend] += 1
+      else
+        by_weekend[:working] += 1
+      end
+    end
+    self
   end
 end
+
+analyze_friends_dates = FriendsDates.analyze(catalog).friends_dates
+making_friends = MakingFriendsData.new(analyze_friends_dates).build
 
 package.workbook.add_worksheet(name: 'Making friends') do |sheet|
   sheet.add_row ['Making friends']
@@ -314,47 +408,49 @@ package.workbook.add_worksheet(name: 'Making friends') do |sheet|
   sheet.add_row ['Making friends by year']
   sheet.add_row ['Year', 'Number of friends added']
 
-  by_year.sort_by { |year, _count| year }.each do |year, count|
+  making_friends.by_year.sort_by { |year, _count| year }.each do |year, count|
     sheet.add_row [year, count]
   end
 
   sheet.add_row ['Making friends by week day']
   sheet.add_row ['Day of week', 'Number of friends added']
 
-  by_week_day.sort_by { |_day, count| count }.reverse.each do |day, count|
+  making_friends.by_week_day.sort_by { |_day, count| count }.reverse.each do |day, count|
     sheet.add_row [day, count]
   end
 
   sheet.add_row ['Making friends by month']
   sheet.add_row ['Month', 'Number of friends added']
 
-  by_month.sort_by { |_month, count| count }.reverse.each do |month, count|
+  making_friends.by_month.sort_by { |_month, count| count }.reverse.each do |month, count|
     sheet.add_row [month, count]
   end
 
   sheet.add_row ['Making friends on weekend vs. working days']
   sheet.add_row ['Working day or weekend', 'Number of friends added']
-  by_weekend.each do |type_of_day, count|
+
+  making_friends.by_weekend.each do |type_of_day, count|
     sheet.add_row [type_of_day.to_s, count]
   end
 
   sheet.add_row ['Most busy weeks for making friends (week number and year)']
   sheet.add_row ['Week and year', 'Number of friends added']
 
-  by_week_and_year.sort_by { |_week_year, count| count }.reverse.each do |week_year, count|
+  making_friends.by_week_and_year.sort_by { |_week_year, count| count }.reverse.each do |week_year, count|
     sheet.add_row [week_year, count]
   end
 
   sheet.add_row ['Most busy month-year by friends added']
   sheet.add_row ['Month year', 'Number of friends added']
-  by_month_and_year.sort_by { |_month_year, count| count }.reverse.each do |month_year, count|
+
+  making_friends.by_month_and_year.sort_by { |_month_year, count| count }.reverse.each do |month_year, count|
     sheet.add_row [month_year, count]
   end
 
   sheet.add_row ['Most busy making friends days']
   sheet.add_row ['Day', 'Number of friends added']
 
-  by_day.sort_by { |_day, count| count }.reverse.each do |day, count|
+  making_friends.by_day.sort_by { |_day, count| count }.reverse.each do |day, count|
     sheet.add_row [day, count]
   end
 end
