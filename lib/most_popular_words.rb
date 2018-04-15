8116d51a4eb1253dbd674fe80699aa0d02011ba4
Eric Bartholemy <eric@kabisa.nl>
2018-04-15 11:14:21 +0200

    fix memoized

--- lib/most_popular_words.rb
+++ lib/most_popular_words.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+# Retrieves the most popular words
 class MostPopularWords
   def self.most_popular_polish_words
-    @popular_polish_words ||= begin
+    @most_popular_polish_words ||= begin
                                 File.open('most_popular_polish_words.txt').map do |line|
                                   line.split(' ')[0].downcase
                                 end.compact
@@ -10,7 +11,7 @@ class MostPopularWords
   end
 
   def self.most_popular_english_words
-    @popular_english_words ||= begin
+    @most_popular_english_words ||= begin
                                  File.open('most_popular_english_words.txt').map do |line|
                                    line.split(' ')[0].downcase
                                  end.compact
