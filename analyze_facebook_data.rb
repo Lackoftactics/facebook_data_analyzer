cb0b1578e2f259de6ef8b9a1bfb6f95c57e4f927
Eric Bartholemy <eric@kabisa.nl>
2018-04-15 11:08:06 +0200

    add comments and extract workbook creation into class and move to lib

--- analyze_facebook_data.rb
+++ analyze_facebook_data.rb
@@ -20,7 +20,12 @@ require_relative 'lib/message_statistics_sheet.rb'
 require_relative 'lib/making_friends_sheet.rb'
 require_relative 'lib/contact_list_sheet.rb'
 require_relative 'lib/vocabulary_statistics_sheet.rb'
+
+# sort of helper
 require_relative 'lib/most_popular_words.rb'
+
+# creates a xlsx file
+require_relative 'lib/workbook.rb'
 
 # images exchanged
 # links exchanged
@@ -43,40 +48,14 @@ require_relative 'lib/most_popular_words.rb'
 # friends gained by month
 # rank everything
 
-analyze_facebook_data = AnalyzeFacebookData.new(ARGV[0]).start
+Workbook.new(catalog: ARGV[0])
 
-# CreatePackage
-package = Axlsx::Package.new
 
-package.workbook.add_worksheet(name: 'Friends ranking') do |sheet|
-  FriendsRankingSheet.build(ranking: analyze_facebook_data.ranking, sheet: sheet)
-end
 
-messages_sent = MessagesSent.new(analyze_facebook_data.my_messages_dates).build
 
-package.workbook.add_worksheet(name: 'My message statistics') do |sheet|
-  MessageStatisticsSheet.build(me: analyze_facebook_data.me, messages_sent: messages_sent, sheet: sheet)
-end
 
-package.workbook.add_worksheet(name: 'Vocabulary statistics') do |sheet|
-  VocabularyStatisticsSheet.build(dictionary: analyze_facebook_data.dictionary,
-                                  me: analyze_facebook_data.me,
-                                  most_popular_english_words: MostPopularWords.most_popular_english_words,
-                                  most_popular_polish_words: MostPopularWords.most_popular_polish_words,
-                                  sheet: sheet)
-end
 
-contact_list = ContactList.new(analyze_facebook_data.catalog).build
 
-package.workbook.add_worksheet(name: 'Contact list') do |sheet|
-  ContactListSheet.build(contacts: contact_list.contacts, sheet: sheet)
-end
 
-analyze_friends_dates = FriendsDates.analyze(analyze_facebook_data.catalog).friends_dates
-making_friends = MakingFriendsData.new(analyze_friends_dates).build
 
-package.workbook.add_worksheet(name: 'Making friends') do |sheet|
-  MakingFriendsSheet.build(making_friends: making_friends, sheet: sheet)
-end
 
-package.serialize('facebook_analysis.xlsx')
