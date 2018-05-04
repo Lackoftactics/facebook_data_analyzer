84a87b630a5aa6112bec618de9b3231b64ed2cfc
Przemyslaw Mroczek <p.mroczek@visuality.pl>
2018-05-04 14:03:54 +0200

    Revert "Adding 3 new Analyses of Messages"

--- analyze_facebook_data.rb
+++ analyze_facebook_data.rb
@@ -3,7 +3,6 @@
 # My script for 'I analyzed my facebook data and it's story of shyness, loneliness and change'
 
 require 'bundler/setup'
-require 'set'
 Bundler.require
 
 require_relative 'classes/analyzeables/analyzeable'
