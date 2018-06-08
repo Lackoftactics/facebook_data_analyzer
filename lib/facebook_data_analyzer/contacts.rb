3d0ace540751a314af37a4574ab2bf35b92e8dbf
Jonathan Northcott <northcott.j@chisigma.co>
2018-06-08 09:59:31 -0400

    missed a merge conflict

--- lib/facebook_data_analyzer/contacts.rb
+++ lib/facebook_data_analyzer/contacts.rb
@@ -36,25 +36,5 @@ module FacebookDataAnalyzer
         end
       end
     end
-<<<<<<< HEAD:lib/facebook_data_analyzer/contacts.rb
-=======
-
-    def export(package:)
-      contact_list_sheet(package: package)
-    end
-
-    private
-
-    def contact_list_sheet(package:)
-      package.workbook.add_worksheet(name: 'Contact list') do |sheet|
-        sheet.add_row ['Contact list']
-        sheet.add_row ["Facebook imported #{@contacts.length} of your contacts"]
-        sheet.add_row ['Name', 'Email']
-        @contacts.sort_by(&:name).each do |contact|
-          sheet.add_row [contact.name, contact.details]
-        end
-      end
-    end
->>>>>>> orig_repo/master:lib/facebook_data_analyzer/analyzeables/contacts.rb
   end
 end
